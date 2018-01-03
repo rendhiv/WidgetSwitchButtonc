e86fc5960588521c6b341f18a921b43310df8efa
guapusing <rendisukirman.rs@gmail.com>
2018-01-03 18:49:49 +0700

    Initial commit

---
+++ app/src/main/java/com/mobiledev/skydev/widgetswitchbutton/MainActivity.java
@@ -0,0 +1,66 @@
+package com.mobiledev.skydev.widgetswitchbutton;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    Switch switchButton, switchButton2;
+    TextView textView, textView2;
+    String switchOn = "Switch is ON";
+    String switchOff = "Switch is OFF";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        switchButton = (Switch) findViewById(R.id.switchButton);
+        textView = (TextView) findViewById(R.id.textView);
+
+        switchButton.setChecked(true);
+        switchButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean bChecked) {
+                if (bChecked) {
+                    textView.setText(switchOn);
+                } else {
+                    textView.setText(switchOff);
+                }
+            }
+        });
+
+        if (switchButton.isChecked()) {
+            textView.setText(switchOn);
+        } else {
+            textView.setText(switchOff);
+        }
+
+        // for second switch button
+        switchButton2 = (Switch) findViewById(R.id.switchButton2);
+        textView2 = (TextView) findViewById(R.id.textView2);
+
+        switchButton2.setChecked(false);
+        switchButton2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean bChecked) {
+                if (bChecked) {
+                    textView2.setText(switchOn);
+                } else {
+                    textView2.setText(switchOff);
+                }
+            }
+        });
+
+        if (switchButton2.isChecked()) {
+            textView2.setText(switchOn);
+        } else {
+            textView2.setText(switchOff);
+        }
+    }
+
+
+}
+
+
+
